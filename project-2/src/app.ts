6d98f1174b1501badedb2899d6438ca59b8814f8
Nazmos27 <nazmos789@gmail.com>
2024-05-15 23:58:50 +0600

    initializing eslint

--- project-2/src/app.ts
+++ project-2/src/app.ts
@@ -1,11 +1,20 @@
-const express = require('express')
+import express, { Request, Response } from 'express'
+import cors from 'cors'
+
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+//parsers
+app.use(express.json())
+app.use(cors())
+
+app.get('/', (req : Request, res : Response) => {
+    var a = 10
+  res.send(a)
 })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+export default app;
