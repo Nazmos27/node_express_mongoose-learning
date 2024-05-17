fba0625307a3def1d1709c2076a2d3808f461501
Nazmos27 <nazmos789@gmail.com>
2024-05-17 11:49:03 +0600

    making interfaces and schema for student

---
+++ project-2/src/app/modules/student.model.ts
@@ -0,0 +1,44 @@
+import { Student } from './student/student.interface';
+import { Schema, model, connect } from 'mongoose';
+
+const studentSchema = new Schema<Student>({
+  id: { type: String },
+  name: {
+    firstName: { type: String, required: true },
+    middleName: { type: String },
+    lastName: { type: String, required: true },
+  },
+  gender : ["male","female"],
+  dateOfBirth : {type : String},
+  email : {type : String, required : true},
+  contactNo : {type : String, required : true},
+  emergencyContactNo : {type : String, required : true},
+  bloodGroup : [
+    "A+",
+    "A-",
+    "O+",
+    "O-",
+    "AB+",
+    "AB-",
+    "B+",
+    "B-",
+  ],
+  presentAdress : {type : String, required : true},
+  permanentAdress : {type : String, required : true},
+  guardian : {
+    fatherName : {type : String, required : true},
+    fatherOccupation : {type : String, required : true},
+    fatherContactNo : {type : String, required : true},
+    motherName : {type : String, required : true},
+    motherOccupation : {type : String, required : true},
+    motherContactNo : {type : String, required : true},
+  },
+  localGuardian : {
+    name : {type : String, required : true},
+    occupation : {type : String, required : true},
+    contactNo : {type : String, required : true},
+    address : {type : String, required : true},
+  },
+  profileImg : {type: String},
+  isActive : ["active","blocked"]
+});
