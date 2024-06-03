9a35c8638e8b9b407928268f374ffd1fcad2ea6a
Nazmos27 <nazmos789@gmail.com>
2024-06-04 00:24:13 +0600

    student deletion functionality implemented

--- project-2/src/app/modules/student/student.service.ts
+++ project-2/src/app/modules/student/student.service.ts
@@ -1,4 +1,8 @@
+import mongoose from 'mongoose';
 import { StudentModel } from './student.model';
+import httpStatus from 'http-status';
+import { UserModel } from '../user/user.model';
+import AppError from '../../errors/AppError';
 
 const getStudentDB = async () => {
   const result = await StudentModel.find()
@@ -15,8 +19,29 @@ const getSingleData = async (id: string) => {
   return result;
 };
 const deleteStudentFromDB = async (id: string) => {
-  const result = await StudentModel.updateOne({ id }, { isDeleted: true });
-  return result;
+
+  const session =await mongoose.startSession()
+
+  try {
+    session.startTransaction()
+    const deletedStudent = await StudentModel.findOneAndUpdate({ id }, { isDeleted: true },{new : true,session})
+    if(!deletedStudent){
+      throw new AppError(httpStatus.NOT_MODIFIED,'Failed to delete student')
+    }
+    const deletedUser = await UserModel.findOneAndUpdate({id},{isDeleted : true}, {new : true,session})
+    if(!deletedUser){
+      throw new AppError(httpStatus.NOT_MODIFIED,'Failed to delete user')
+    }
+    await session.commitTransaction()
+    await session.endSession()
+
+    return deletedStudent;
+  } catch (error) {
+    await session.abortTransaction()
+    await session.endSession()
+    throw new AppError(httpStatus.INTERNAL_SERVER_ERROR,'Failed to delete')
+  }
+  
 };
 
 export const StudentServices = {
