79cb444e4d4d780d4d3a57ba051a9125a52b6326
Nazmos27 <nazmos789@gmail.com>
2024-06-10 15:53:48 +0600

    interface,model,validaton for semReg done

--- project-2/src/app/modules/SemesterRegistration/semesterRegistration.validation.ts
+++ project-2/src/app/modules/SemesterRegistration/semesterRegistration.validation.ts
@@ -1,4 +1,12 @@
 import Joi from "joi";
+import { semesterRegistrationStatus } from "./semesterRegistration.constant";
 
-export const createSemesterRegistrationValidationSchema = Joi.object()
+export const createSemesterRegistrationValidationSchema = Joi.object({
+    academicSemester: Joi.string().required(),
+  status: Joi.string().valid(...semesterRegistrationStatus).default('UPCOMING'),
+  startDate: Joi.date().required(),
+  endDate: Joi.date().required(),
+  minCredit: Joi.number().default(3),
+  maxCredit: Joi.number().default(15),
+})
 
