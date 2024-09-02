2fa33213c41aa9c5139d49c9e41c5cc3addef16f
Nazmos27 <nazmos789@gmail.com>
2024-09-02 23:41:37 +0600

    intializing super admin function

--- project-2/src/app/modules/user/user.model.ts
+++ project-2/src/app/modules/user/user.model.ts
@@ -29,7 +29,7 @@ const userSchema = new Schema<TUser, UserModelInterface>(
     },
     role: {
       type: String,
-      enum: ['student', 'faculty', 'admin'],
+      enum: ['super-admin', 'student', 'faculty', 'admin'],
     },
     status: {
       type: String,
